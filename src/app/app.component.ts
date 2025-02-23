f0bd18db41de75bea204c7eb35c1570e0051030d
mohsen77sk <mohsen77sk@gmail.com>
2025-02-23 16:13:42 +0330

    add material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  encapsulation: ViewEncapsulation.None,
+  imports: [RouterOutlet],
 })
 export class AppComponent {
   title = 'material-tailwind4';
